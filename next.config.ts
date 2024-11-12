c08e45ceb7aec83fab13ab31bee5453ef78f1b96
Taizeng Wu <wutaizeng@gmail.com>
2024-11-12 16:01:17 +0800

    chore: add cloudflare pages deployment config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
+import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
 };
+
+if (process.env.NODE_ENV === 'development') {
+  await setupDevPlatform();
+}
 
 export default nextConfig;
